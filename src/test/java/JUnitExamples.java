0a27e37eaa5a6e8a0e072db7cf5838585d2649f0
Evgenii <sergeevjane@mail.ru>
2023-05-13 23:23:03 +0300

    added JUnitExamples

---
+++ src/test/java/JUnitExamples.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.*;
+
+public class JUnitExamples {
+
+    @BeforeAll
+    static void beforeAll() {
+    System.out.println("Here is beforeAll()");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("    Here is beforeEach()");
+    }
+
+    @AfterAll
+    static void AfterAll() {
+        System.out.println("Here is afterAll()");
+    }
+
+    @AfterEach
+    void AfterEach() {
+        System.out.println("    Here is afterEach()");
+    }
+    @Test
+    void firstTest() {
+        System.out.println("        Here is firstTest()");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("            Here is secondTest()");
+    }
+}
