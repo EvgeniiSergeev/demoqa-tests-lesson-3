16f04c9c2c4a55cff520be5d98aba89816610788
Evgenii <sergeevjane@mail.ru>
2023-05-21 19:24:52 +0300

    added RegistrationTests

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -19,7 +19,7 @@ public class RegistrationTests {
     }
 
         @Test
-        void successfulRegistrationTest {
+        void successfulRegistrationTest() {
             String userName = "Evgenii";
 
             open("/automation-practice-form");
@@ -30,17 +30,19 @@ public class RegistrationTests {
             $("#firstName").setValue(userName); // [id=userName] = #userName [class=userName] = .userName
             $("#lastName").setValue("Bokov");
             $("#userEmail").setValue("evgenii@mail.ru");
-            $("#gender-radio-1").parent().click(); // ищем радиокнопку через родителя и тапаем на нее
-//        $("#genterWrapper").$(byText("Female")).click(); не очень хороший пример с выбором кнопки но для определ локали
-//        $("label[for="gender-radio-3]").click(); ищем радиокнопку через лэйбл и тапаем на нее
-            $("#userNumber").setValue("79995552211");
-            $("#dateOfBirthInput").click(); // выбор даты рождения в календаре
-            $(".react-datepicker__month-select").selectOption("September");
+//        $("#gender-radio-1").click(); // wrong
+//        $("#gender-radio-3").parent().click(); // ищем радиокнопку через родителя и тапаем на нее
+//        $(byText("Other")).click(); // not very good
+            $("#genterWrapper").$(byText("Other")).click(); // не очень хороший пример с выбором кнопки для определ локали
+//        $("label[for=gender-radio-3]").click(); // ищем радиокнопку через лэйбл и тапаем на нее
+            $("#userNumber").setValue("1234567890");
+            $("#dateOfBirthInput").click(); //// выбор даты рождения в календаре
+            $(".react-datepicker__month-select").selectOption("September");  // выбираем день в месяце
 //        $(".react-datepicker__month-select").selectOptionByValue("6");
             $(".react-datepicker__year-select").selectOption("1988");
-            $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click(); // выбираем день в месяце
-// <div class="react-datepicker__day--030 react-datepicker__day--outside-month">30</div>
-// <div class="react-datepicker__day--030"                                     >30</div>
+//        <div class="react-datepicker__day--030 react-datepicker__day--outside-month">30</div>
+//        <div class="react-datepicker__day--030"                                     >30</div>
+//        $x("//*[@class='react-datepicker__day--030'][not(contains(@class, 'react-datepicker__day--outside-month'))]").click();
             $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
 //        $x("//*[@class='react-datepicker__day--030'][not(contains(@class, 'react-datepicker__day--outside-month'))]").click();
             $("#subjectsInput").setValue("Math").pressEnter();
@@ -59,9 +61,6 @@ public class RegistrationTests {
             $(".modal-dialog").should(appear);
             $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
             $(".table-responsive").shouldHave(text(userName), text("Bokov"),
-                    text("evgenii@mail.ru"), text("79995552211"));
+                    text("evgenii@mail.ru"), text("1234567890"));
         }
-
 }
-
-
